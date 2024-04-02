c895093d6eea664c02ab447f70cdc77340716dc7
JustFiesta <mateusz.bocak9@gmail.com>
2024-04-02 13:57:57 +0200

    model fix for http methods

--- models/models.go
+++ models/models.go
@@ -1,13 +1,17 @@
 package models
 
 type User struct {
-    id       uint   `gorm:"primaryKey"`
-    user_name string `gorm:"unique"`
+	ID       uint   `gorm:"primaryKey"`
+	UserName string `gorm:"unique"`
+}
+
+func (User) TableName() string {
+	return "user" // change def gorm tablename to user
 }
 
 type Location struct {
-    id        uint    `gorm:"primaryKey"`
-    user_id    uint    `gorm:"index"`
-    latitude  float64 `json:"latitude"`
-    longitude float64 `json:"longitude"`
+	id        uint    `gorm:"primaryKey"`
+	user_id   uint    `gorm:"index"`
+	latitude  float64 `json:"latitude"`
+	longitude float64 `json:"longitude"`
 }
