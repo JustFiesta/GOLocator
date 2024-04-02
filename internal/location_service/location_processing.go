2fbfe85ae48b5475ea1526e73ea28cdc6dc90f43
JustFiesta <mateusz.bocak9@gmail.com>
2024-04-02 13:32:33 +0200

    location api sketch

--- internal/location_service/location_processing.go
+++ internal/location_service/location_processing.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+var db *gorm.DB
+
+func initDB() {
+	dsn := "root:@tcp(localhost:3306)/golocator?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+}
+
+func getTraveledDistance(c echo.Context) error{
+
+}
 
 func runLocationService() {
 	e := echo.New()
@@ -16,4 +35,23 @@ func runLocationService() {
 
 func main() {
 	runLocationService()
+
+	e := echo.New()
+
+	// logger from echo doc
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus: true,
+		LogURI:    true,
+		BeforeNextFunc: func(c echo.Context) {
+			c.Set("customValueFromContext", 42)
+		},
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			value, _ := c.Get("customValueFromContext").(int)
+			fmt.Printf("REQUEST: uri: %v, status: %v, custom-value: %v\n", v.URI, v.Status, value)
+			return nil
+		},
+	}))
+
+	e.GET("/locationhistory/distancetraveled", getTraveledDistance)
+	e.Logger.Fatal(e.Start(":1323"))
 }
